665b3b5ef26a57f8018aad7bcaf400b52512ef0a
Matthew Moore <matt@crowdmob.com>
2012-02-27 18:22:44 -0800

    base

---
+++ lib/dynamodb-authur-client.rb
@@ -0,0 +1,9 @@
+require "dynamodb-authur-client/version"
+
+module Dynamodb
+  module Authur
+    module Client
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/dynamodb-authur-client/version.rb
@@ -0,0 +1,7 @@
+module Dynamodb
+  module Authur
+    module Client
+      VERSION = "0.0.1"
+    end
+  end
+end
